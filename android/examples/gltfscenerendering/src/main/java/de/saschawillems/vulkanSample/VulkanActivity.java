70c72fb5d35ca17a6454468f868422773e422852
Hakim Hauston <hakimh@google.com>
2024-08-26 12:54:15 +0800

    [ADPF] game activity integration - fix the build issues! Thanks Nate!

--- android/examples/gltfscenerendering/src/main/java/de/saschawillems/vulkanSample/VulkanActivity.java
+++ android/examples/gltfscenerendering/src/main/java/de/saschawillems/vulkanSample/VulkanActivity.java
@@ -30,31 +30,31 @@ public class VulkanActivity extends GameActivity {
 
     private final Semaphore semaphore = new Semaphore(0, true);
 
-    // public void showAlert(final String message)
-    // {
-    //     final VulkanActivity activity = this;
+    public void showAlert(final String message)
+    {
+        final VulkanActivity activity = this;
 
-    //     ApplicationInfo applicationInfo = activity.getApplicationInfo();
-    //     final String applicationName = applicationInfo.nonLocalizedLabel.toString();
+        ApplicationInfo applicationInfo = activity.getApplicationInfo();
+        final String applicationName = applicationInfo.nonLocalizedLabel.toString();
 
-    //     this.runOnUiThread(new Runnable() {
-    //        public void run() {
-    //            AlertDialog.Builder builder = new AlertDialog.Builder(activity, android.R.style.Theme_Material_Dialog_Alert);
-    //            builder.setTitle(applicationName);
-    //            builder.setMessage(message);
-    //            builder.setPositiveButton("Close", new DialogInterface.OnClickListener() {
-    //                public void onClick(DialogInterface dialog, int id) {
-    //                    semaphore.release();
-    //                }
-    //            });
-    //            builder.setCancelable(false);
-    //            AlertDialog dialog = builder.create();
-    //            dialog.show();
-    //        }
-    //     });
-    //     try {
-    //         semaphore.acquire();
-    //     }
-    //     catch (InterruptedException e) { }
-    // }
+        this.runOnUiThread(new Runnable() {
+           public void run() {
+               AlertDialog.Builder builder = new AlertDialog.Builder(activity, android.R.style.Theme_Material_Dialog_Alert);
+               builder.setTitle(applicationName);
+               builder.setMessage(message);
+               builder.setPositiveButton("Close", new DialogInterface.OnClickListener() {
+                   public void onClick(DialogInterface dialog, int id) {
+                       semaphore.release();
+                   }
+               });
+               builder.setCancelable(false);
+               AlertDialog dialog = builder.create();
+               dialog.show();
+           }
+        });
+        try {
+            semaphore.acquire();
+        }
+        catch (InterruptedException e) { }
+    }
 }
